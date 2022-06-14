f144830fa85b956095daa8d4339f40e127cb3559
Abantika Karmakar <ak83@iitbbs.ac.in>
2022-06-14 12:16:01 +0530

    Initial Commit

---
+++ src/component/menuApi.js
@@ -0,0 +1,75 @@
+const Menu = [{
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price : "20₹",
+    description: "This iconic brand is on a mission to champion the goodness of home cooking, renewing its global product portfolio with ingredients that people are familiar with, like those they might find in their kitchen cupboard. That means more vegetables and original flavours from vegetables, herbs and spices, grains and other nutrient-rich ingredients.",
+    },
+    {
+        id: 2,
+        image: "../images/aloo.jpg",
+        name: "allu pakoida",
+        category: "evening",
+        price: "20₹",
+        description: "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes. Aloo pakora is a popular street food snack served not only in Mumbai but in other parts of India as well. There was a popular vendor who made the tastiest pakoras near my office."
+    },
+    {
+        id: 3,
+        image: "../images/corn.jpg",
+        name: "corn",
+        category: "breakfast",
+        price: "10₹",
+        description: "Corn, (Zea mays), also called Indian corn or maize, cereal plant of the grass family (Poaceae) and its edible grain. The domesticated crop originated in the Americas and is one of the most widely distributed of the world food crops. Corn is used as livestock feed, as human food, as biofuel, and as raw material in industry. Corn is the third largest plant-based food source in the world. ."
+    },
+    {
+        id: 4,
+        image: "../images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "50₹",
+        description: "Chana chaat is a tasty, tangy and easy variety of chaat snack made with white chickpeas (chana or chole), spices, herbs, onions, tomatoes and lemon. This chaat is quick to assemble if you have canned or leftover boiled chickpeas. If using canned chickpeas, then you just need to mix the spices, potatoes, onions, tomatoes etc and savor the chana chaat."
+    },
+    {
+        id: 5,
+        image: "../images/pizza.jpg",
+        name: 'pizza',
+        category: "evening",
+        price: "80₹",
+        description: "Pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot."
+    },
+    {
+        id: 6,
+        image: "../images/thali.jpg",
+        name: "non-veg thali",
+        category: "lunch",
+        price: "100₹",
+        description: "Non-vegetarian food is a term commonly used in India for food that has meat, fish or eggs in it. It is more commonly called non-veg food. Not just for food, it can be used for a person too - a person eating meat is a non-vegetarian in Indian English. Egg too is not classified as vegetarian for Indians. Well, they are called eggitarians."
+    },
+    {
+        id: 7,
+        image: "../images/rajma.jpg",
+        name: "rajma chawal",
+        category: "dinner",
+        price: "60₹",
+        description: "This simple Indian dish consists of red beans (rajma) and cooked rice (chawal). Rajma is traditionally made with onions and tomato purée which acts as a gravy for the red beans. This dish makes for a complete vegetarian meal that is suitable for lunch or dinner."
+    },
+    {
+        id: 8,
+        image: "../images/sweets.jpg",
+        name: "sweets",
+        category: "dinner",
+        price: "50₹",
+        description: "We Indians are known to have a sweet tooth. All of us have a great love towards the varieties of sweets.  From auspicious occasion to festivals, sweets is the most important part of our any celebration. With the presence of delectable desserts and sweets any celebration such as marriage, birthdays parties, etc. seems to be incomplete."
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "samosa",
+        category: "evening",
+        price: "10₹",
+        description: "If you think that the triangular version of samosa, filled with mashed potatoes is the only way you can enjoy this savoury snack, then you need to know that there are more than 15-20 desi avatars of samosa being relished pan-India. In Hyderabad, it is called Lukhmi, which has a thicker crust and is stuffed with minced meat. While in South India, samosas are prepared with cabbage, carrots and curry leaves as stuffing."
+    }
+];
+
+export default Menu;
